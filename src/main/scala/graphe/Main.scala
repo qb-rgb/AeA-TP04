da159d54332f864ed0ec936741d782dbf8572558
qw0te <baert.quentin@gmail.com>
2015-03-20 14:48:03 +0100

    Ajoute une classe Main pour les tests rapides

---
+++ src/main/scala/graphe/Main.scala
@@ -0,0 +1,25 @@
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val vertexes = (for (id <- 'A' to 'H') yield Vertex(id)).toSet
+    val vertexesId = (vertexes map (v => v.id -> v)).toMap
+    val edges = Set(
+      Edge(vertexesId('A'), vertexesId('B'), 6),
+      Edge(vertexesId('A'), vertexesId('H'), 14),
+      Edge(vertexesId('A'), vertexesId('C'), 5),
+      Edge(vertexesId('A'), vertexesId('G'), 8),
+      Edge(vertexesId('B'), vertexesId('C'), 12),
+      Edge(vertexesId('C'), vertexesId('F'), 7),
+      Edge(vertexesId('C'), vertexesId('D'), 9),
+      Edge(vertexesId('E'), vertexesId('F'), 15),
+      Edge(vertexesId('F'), vertexesId('G'), 10),
+      Edge(vertexesId('G'), vertexesId('H'), 3),
+      Edge(vertexesId('H'), vertexesId('G'), 3)
+    )
+
+    val graphe = new Graphe(vertexes, edges)
+
+    println(graphe.toString)
+  }
+
+}
